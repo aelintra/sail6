686aaa6c531556c518521219634a1baf7d9c27c1
admin <admin@aelintra.com>
2020-12-23 22:41:44 +0000

    tailoring 6.2

--- sail-6/opt/sark/scripts/srkagerecordings.sh
+++ sail-6/opt/sark/scripts/srkagerecordings.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 #
 # delete recordings older than $RECAGE days
 #
-RECAGE=`cat /opt/sark/.recage`
-if ! [[ "$RECAGE" =~ ^[0-9]+$ ]]
-    then
-        RECAGE = "60"
-fi
-find /opt/sark/www/origrecs/recordings/*  -mtime +$RECAGE -type d -exec rm -rf {} +
+
+RECAGE=`/usr/bin/sqlite3 /opt/sark/db/sark.db "select RECAGE from globals;"`
+
+echo find /opt/sark/www/origrecs/recordings/*  -mtime +$RECAGE -type d -exec rm -rf {} +
+
