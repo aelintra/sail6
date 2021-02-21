14db142fa44a4032689fabced2a8044317e2b96e
aelintra <admin@aelintra.com>
2021-02-21 20:40:29 +0000

    refactor splash

--- sail-6/opt/sark/php/sarksplash/view.php
+++ sail-6/opt/sark/php/sarksplash/view.php
@@ -139,89 +139,25 @@ private function showMain() {
 
 	echo '</div>';
 	
-	
-/*
-
-  	echo '<div class="w3-container w3-padding ' . $this->myPanel->bgColorClass . '" >';
-  	echo '<div class="w3-col w3-quarter">';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-	echo '<div class="w3-display-container w3-card-4 w3-half w3-round-large' . $boxcolor . '" style="height:6em">';
-  	echo '<div class="w3-display-topmiddle w3-padding w3-small">Extensions</div>';
-  	echo '<div id="extensions" class="w3-display-middle w3-xlarge">0/0</div>';
-  	echo '<div class="w3-display-bottommiddle w3-padding w3-small">up</div>';
-  	echo '</div>';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-  	echo '</div>';
-
-  	echo '<div class="w3-col w3-quarter">';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-	echo '<div class="w3-display-container w3-card-4 w3-half w3-round-large' . $boxcolor . '" style="height:6em">';
-  	echo '<div class="w3-display-topmiddle w3-padding w3-small">Trunks</div>';
-  	echo '<div id="trunks" class="w3-display-middle w3-xlarge">0/0</div>';
-  	echo '<div class="w3-display-bottommiddle w3-padding w3-small">up</div>';
-  	echo '</div>';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-  	echo '</div>';
-
-  	if (file_exists("/opt/sark/cache/speedtest")) {
-		$downlink = `/bin/grep Down /opt/sark/cache/speedtest`;
-		$uplink = `/bin/grep Up /opt/sark/cache/speedtest`;
-		if ($downlink) {
-			$speed = explode(':',$downlink); 
-			$num  = explode(' ',$speed[1]);
-			$down = $num[1];
-		}
-		if ($uplink) {
-			$speed = explode(':',$uplink); 
-			$num  = explode(' ',$speed[1]); 
-			$up = $num[1];
-		} 
-	}
- 
-  	echo '<div class="w3-col w3-quarter">';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-	echo '<div class="w3-display-container w3-card-4 w3-half w3-round-large' . $boxcolor . '" style="height:6em">';
-  	echo '<div class="w3-display-topmiddle w3-padding w3-small">Uplink</div>';
-  	echo '<div class="w3-display-middle w3-xlarge">' . $up . '<sup>*</sup></div>';
-  	echo '<div class="w3-display-bottommiddle w3-padding w3-small">Mbps</div>';
-  	echo '</div>';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-  	echo '</div>';
-
-  	echo '<div class="w3-col w3-quarter">';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-	echo '<div class="w3-display-container w3-card-4 w3-half w3-round-large' . $boxcolor . '" style="height:6em">';
-  	echo '<div class="w3-display-topmiddle w3-padding w3-small">Downlink</div>';
-  	echo '<div class="w3-display-middle w3-xlarge">' . $down . '<sup>*</sup></div>';
-  	echo '<div class="w3-display-bottommiddle w3-padding w3-small">Mbps</div>';
-  	echo '</div>';
-	echo '<div class="w3-col w3-quarter">&nbsp;</div>';
-  	echo '</div>';
-
-*/
 
   	echo '<div class="w3-row-padding w3-light-gray" style="min-height:34em;">' . PHP_EOL;
 	echo '<div class="w3-col w3-container w3-hide-small m1 l2">&nbsp;</div>' . PHP_EOL;
     echo '<div class="w3-col w3-container m10 l8">' . PHP_EOL;
-    echo '<br/><br/>';
+    echo '<br/>';
 	$this->myPanel->aLabelFor("Live Calls...");
-	echo '<br/><br/>';
+	echo '<br/>';
 	echo '<table class="' . $this->myPanel->tableClass . ' w3-white w3-small" id="chantable"></table>';
 	echo '<br/>';
 	echo '</div>';
   	echo '</div>'; 
 
-/*
-	echo '<br/><br/><br/>';
-	echo '<br/><br/><br/>';
- */
 	echo '</div>';
 
 	
 //  	$this->myPanel->printSysNotes();
 
   	
-//  	echo '<div class="w3-tiny w3-padding ' . $this->myPanel->bgColorClass . '"><sup>*</sup>Internet speeds are as observed from the PBX and will usually be somewhat lower than the advertised speed of the circuit.</div>';
+  	echo '<div class="w3-tiny w3-padding ' . $this->myPanel->bgColorClass . '"><sup>*</sup>Internet speeds are as observed from the PBX and will usually be somewhat lower than the advertised speed of the circuit.</div>';
 
   	echo '</div>';
   	echo '</div>'; 
