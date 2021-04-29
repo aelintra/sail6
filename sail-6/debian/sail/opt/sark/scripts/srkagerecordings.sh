3d80063ac4090dca2213e860d14e991efaf7f1a4
aelintra <admin@aelintra.com>
2021-04-29 15:51:23 +0100

    remove debian build artifacts

--- sail-6/debian/sail/opt/sark/scripts/srkagerecordings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# delete recordings older than $RECAGE days
-#
-
-RECAGE=`/usr/bin/sqlite3 /opt/sark/db/sark.db "select RECAGE from globals;"`
-
-find /opt/sark/www/origrecs/recordings/*  -mtime +$RECAGE -type d -exec rm -rf {} +
-
