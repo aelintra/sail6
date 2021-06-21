dd1a65bedc5f8daed582d6de147c13c7e4dec5cb
aelintra <admin@aelintra.com>
2021-06-21 21:10:23 +0100

    moh backport from 7.x

--- sail-6/opt/sark/php/sarkcluster/javascript.js
+++ sail-6/opt/sark/php/sarkcluster/javascript.js
@@ -1,6 +1,17 @@
 
   $(document).ready(function() {
 
+  	$("[name='newmoh']").click(function() {
+		$('#file').click();
+	});
+	
+	
+	
+	$('#file').change(function() {
+		$('#newmohclick').val("TRUE");
+		$('#sarkclusterForm').submit();
+	});	
+
 	$.validator.addMethod("tenant",function(value,element) {
 		return this.optional(element) || /^[A-Za-z0-9-_]{2,30}$/i.test(value); 
 	},"tenant format is [A-Za-z0-9-_]{2,30} i.e no spaces or specal characters, max length 30 chars");
