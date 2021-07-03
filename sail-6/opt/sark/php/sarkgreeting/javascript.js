b86fda66aba108b996e9848391715399aa707cb8
aelintra <admin@aelintra.com>
2021-07-03 16:50:53 +0100

    backport sysSelect from V7

--- sail-6/opt/sark/php/sarkgreeting/javascript.js
+++ sail-6/opt/sark/php/sarkgreeting/javascript.js
@@ -47,7 +47,7 @@
 			"sSearch": "Filter:"
 		},		
 		"fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-          $('td:eq(1),td:eq(2)', nRow).addClass( "bluetags" );
+          $('td:eq(1),td:eq(2)', nRow).addClass( "w3-text-blue" );
         }  
 
 	} ).makeEditable({
