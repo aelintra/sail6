028849781000a4a208b748f9e044b5770dfafed5
aelintra <admin@aelintra.com>
2021-08-26 08:44:05 -0400

    6.2.0-17 build

--- sail-6/opt/sark/scripts/srkoffload_simple.sh
+++ sail-6/opt/sark/scripts/srkoffload_simple.sh
@@ -1,3 +1,7 @@
 date >>/var/log/recdump.log
 #set this rsync to point to your offload or nearline media
-rsync  --remove-source-files -a /var/spool/asterisk/monout/* /opt/sark/www/origrecs/recordings/`date +%d%m%y`/ >>/var/log/recdump.log 2>&1
+#create directory tree if it doesn't exist
+#
+[ ! -d /opt/sark/media/recordings/default ] && mkdir -p /opt/sark/media/recordings/default
+#Move the files
+rsync  --remove-source-files -a /var/spool/asterisk/monout/* /opt/sark/media/recordings/default`date +%d%m%y`/ >>/var/log/recdump.log 2>&1
