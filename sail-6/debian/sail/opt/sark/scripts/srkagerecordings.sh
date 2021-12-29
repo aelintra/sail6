fb9550b8c5f001d3a9076f1496da24d01c3ba117
Aelintra <admin@aelintra.com>
2021-12-29 10:26:10 +0000

    ignore debian work files

--- sail-6/debian/sail/opt/sark/scripts/srkagerecordings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# delete recordings older than $RECAGE days
-#
-
-RECAGE=`/usr/bin/sqlite3 /opt/sark/db/sark.db "select RECAGE from globals;"`
-
-find /opt/sark/media/recordings  -mtime +$RECAGE -type d -exec rm -rf {} +
-
--- sail-6/debian/sail/opt/sark/scripts/srkoffload_simple.sh
+++
@@ -1,7 +0,0 @@
-date >>/var/log/recdump.log
-#set this rsync to point to your offload or nearline media
-#create directory tree if it doesn't exist
-#
-[ ! -d /opt/sark/media/recordings/default ] && mkdir -p /opt/sark/media/recordings/default
-#Move the files
-rsync  --remove-source-files -a /var/spool/asterisk/monout/* /opt/sark/media/recordings/default/`date +%d%m%y`/ >>/var/log/recdump.log 2>&1
