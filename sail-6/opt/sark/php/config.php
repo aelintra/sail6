ce8816cccd17ab0dabb91c67557067d0cf07c979
aelintra <admin@aelintra.com>
2022-07-09 16:59:44 -0400

    pjsip wip

--- sail-6/opt/sark/php/config.php
+++ sail-6/opt/sark/php/config.php
@@ -7,12 +7,16 @@
 define('PJSIP', 						'/etc/asterisk/sark_pjsip_');
 define('PJSIP_ANONYMOUS',				'anonymous.conf');
 define('PJSIP_GLOBALS', 				'globals.conf');
-define('PJSIP_PHONE',					'phone.conf');	  
-define('PJSIP_PHONE_TEMPLATE',			'phone_template.conf');	      
+
+define('PJSIP_PHONE',					'phone.conf');
+define('PJSIP_PHONE_TEMPLATE',          'phone.tmpl');
+define('PJSIP_READY_PHONES',            'ready_phones.conf');
+	      
 define('PJSIP_TRANSPORT',				'transport.conf');	
-define('PJSIP_TRANSPORT_TEMPLATE',		'transport_template.conf');
-define('PJSIP_READY_TRUNKS',            'ready_trunks.conf');
+define('PJSIP_TRANSPORT_TEMPLATE',		'transport.tmpl');
+
 define('PJSIP_TRUNK',					'trunk.conf');
-define('PJSIP_TRUNK_SNDREG_TEMPLATE',	'trunk_sndreg_template.conf');
-define('PJSIP_TRUNK_RCVREG_TEMPLATE',	'trunk_rcvreg_template.conf');
-define('PJSIP_TRUNK_TRUSTED_TEMPLATE',	'trunk_trusted_template.conf');
+define('PJSIP_TRUNK_SNDREG_TEMPLATE',	'trunk_sndreg.tmpl');
+define('PJSIP_TRUNK_RCVREG_TEMPLATE',	'trunk_rcvreg.tmpl');
+define('PJSIP_TRUNK_TRUSTED_TEMPLATE',	'trunk_trusted.tmpl');
+define('PJSIP_READY_TRUNKS',            'ready_trunks.conf');
