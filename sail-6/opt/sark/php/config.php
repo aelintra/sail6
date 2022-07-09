1a8ab70b264ce53e228ece40fa969cc7b6d5c306
aelintra <admin@aelintra.com>
2022-07-09 14:44:06 -0400

    pjsip wip

--- sail-6/opt/sark/php/config.php
+++ sail-6/opt/sark/php/config.php
@@ -11,6 +11,7 @@ define('PJSIP_PHONE',					'phone.conf');
 define('PJSIP_PHONE_TEMPLATE',			'phone_template.conf');	      
 define('PJSIP_TRANSPORT',				'transport.conf');	
 define('PJSIP_TRANSPORT_TEMPLATE',		'transport_template.conf');
+define('PJSIP_READY_TRUNKS',            'ready_trunks.conf');
 define('PJSIP_TRUNK',					'trunk.conf');
 define('PJSIP_TRUNK_SNDREG_TEMPLATE',	'trunk_sndreg_template.conf');
 define('PJSIP_TRUNK_RCVREG_TEMPLATE',	'trunk_rcvreg_template.conf');
